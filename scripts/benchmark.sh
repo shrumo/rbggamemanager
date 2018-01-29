a58b68d5f443f835f703bd34c9228d36c36907c0
shrum <mika.maksymilian@gmail.com>
2018-01-29 16:24:30 +0100

    Added perft calculation just for the first state.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,25 +2,36 @@
 
 cd "${0%/*}"
 ./make.sh
-echo -e "\n\tChess (Depth: 0)"
-../build/rbggamemanager ../rbgParser/examples/chess.rbg -n 2 -s 0 -d 0
-echo -e "\n\tCheckers (Depth: 0)"
-../build/rbggamemanager ../rbgParser/examples/checkers.rbg -n 10 -s 0 -d 0
-echo -e "\n\tReversi (Depth: 0)"
-../build/rbggamemanager ../examples/reversi.rbg -n 10 -s 0 -d 0
-echo -e "\n\tBigReversi (Depth: 0)"
-../build/rbggamemanager ../examples/big_reversi.rbg -n 10 -s 0 -d 0
-echo -e "\n\tBreakthrough (Depth: 0)"
-../build/rbggamemanager ../examples/breakthrough.rbg -n 10 -s 0 -d 0
-echo -e "\n\tAmazons (Depth: 0)"
-../build/rbggamemanager ../examples/amazons.rbg -n 10 -s 0 -d 0
-echo -e "\n\tConnect4 (Depth: 0)"
-../build/rbggamemanager ../examples/connect4.rbg -n 10 -s 0 -d 0
-echo -e "\n\tGo (Depth: 0)"
-../build/rbggamemanager ../examples/go.rbg -n 2 -s 0 -d 0
-echo -e "\n\tDouble Chess (Depth: 0)"
-../build/rbggamemanager ../examples/doublechess.rbg -n 2 -s 0 -d 0
-echo -e "\n\tEngdraughts (Depth: 0)"
-../build/rbggamemanager ../examples/engdraughts.rbg -n 2 -s 0 -d 0
-echo -e "\n\tEngdraughts Split (Depth: 0)"
-../build/rbggamemanager ../examples/engdraughts-split.rbg -n 2 -s 0 -d 0
+echo -e "\n\tChess"
+../build/rbggamemanager ../rbgParser/examples/chess.rbg -n 2 -s 0 
+../build/rbggamemanager ../rbgParser/examples/chess.rbg -s 0 -d 3
+echo -e "\n\tCheckers"
+../build/rbggamemanager ../rbgParser/examples/checkers.rbg -n 10 -s 0 
+../build/rbggamemanager ../rbgParser/examples/checkers.rbg -s 0 -d 3
+echo -e "\n\tReversi"
+../build/rbggamemanager ../examples/reversi.rbg -n 10 -s 0 
+../build/rbggamemanager ../examples/reversi.rbg -s 0 -d 3
+echo -e "\n\tBigReversi"
+../build/rbggamemanager ../examples/big_reversi.rbg -n 10 -s 0 
+../build/rbggamemanager ../examples/big_reversi.rbg -s 0 -d 3
+echo -e "\n\tBreakthrough"
+../build/rbggamemanager ../examples/breakthrough.rbg -n 10 -s 0 
+../build/rbggamemanager ../examples/breakthrough.rbg -s 0 -d 3
+echo -e "\n\tAmazons"
+../build/rbggamemanager ../examples/amazons.rbg -n 10 -s 0 
+../build/rbggamemanager ../examples/amazons.rbg -s 0 -d 2
+echo -e "\n\tConnect4"
+../build/rbggamemanager ../examples/connect4.rbg -n 10 -s 0 
+../build/rbggamemanager ../examples/connect4.rbg -s 0 -d 3
+echo -e "\n\tGo"
+../build/rbggamemanager ../examples/go.rbg -n 2 -s 0 
+../build/rbggamemanager ../examples/go.rbg -s 0 -d 2 
+echo -e "\n\tDouble Chess"
+../build/rbggamemanager ../examples/doublechess.rbg -n 2 -s 0 
+../build/rbggamemanager ../examples/doublechess.rbg -s 0 -d 3
+echo -e "\n\tEngdraughts"
+../build/rbggamemanager ../examples/engdraughts.rbg -n 2 -s 0 
+../build/rbggamemanager ../examples/engdraughts.rbg -s 0 -d 3
+echo -e "\n\tEngdraughts Split"
+../build/rbggamemanager ../examples/engdraughts-split.rbg -n 2 -s 0 
+../build/rbggamemanager ../examples/engdraughts-split.rbg -s 0 -d 3
--- src/main.cpp
+++ src/main.cpp
@@ -26,78 +26,28 @@ struct player_results
     int sum,min,max;
 };
 
-int perft(search_context* context, game_state *state, size_t depth)
+size_t perft(search_context* context, game_state *state, size_t depth)
 {
     if(depth == 0)
         return 1;
     auto moves = state->find_moves(context);
-    int result = 0;
+    size_t result = 0;
     for(const auto& move : moves) {
         auto revert_info = state->make_revertible_move(move);
-        int rec_res = perft(context, state, depth - 1);
+        size_t rec_res = perft(context, state, depth - 1);
         result += rec_res;
         state->revert_move(revert_info);
     }
     return result;
 }
 
-int main(int argc,const char *argv[]) {
-    po::options_description description("Allowed options");
-    description.add_options()
-            ("number,n",po::value<uint>(),"number of games to be played")
-            ("help,h","produce help message")
-            ("input-file,i",po::value<std::string>(),"input file")
-            ("randomseed,s",po::value<uint>(),"random seed for random player")
-            ("depth,d",po::value<uint>(),"depth of perft calculation")
-            ;
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc,argv,description), vm);
-    po::notify(vm);
-
-    po::positional_options_description p;
-    p.add("input-file", -1);
-
-    po::store(po::command_line_parser(argc, argv).
-            options(description).positional(p).run(), vm);
-    po::notify(vm);
-
-    if(vm.count("randomseed")) {
-        srand(vm["randomseed"].as<uint>());
-    }
-    if(vm.count("help")) {
-        std::cout << description << std::endl;
-        return 1;
-    }
-
-    size_t search_depth = 0;
-    if(vm.count("depth"))
-    {
-        search_depth = vm["depth"].as<uint>();
-    }
-
-    rbg_parser::messages_container msg;
-    std::ifstream t(vm["input-file"].as< std::string>());
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-
-    std::unique_ptr<rbg_parser::parsed_game> pg;
-
-    try {
-        std::vector<rbg_parser::token> result = tokenize(buffer.str(), msg);
-        rbg_parser::game_items g = input_tokens(result, msg);
-        pg = std::unique_ptr<rbg_parser::parsed_game>(new rbg_parser::parsed_game(g.parse_game(msg)));
-    }catch(rbg_parser::message& m){
-        std::cout<<"Game description is bad. Here is the error: " << "\n";
-        std::cout<<m.as_error()<<std::endl;
-        return 1;
-    }
-
-    game_description gd(create_description(*pg));
+void random_play_benchmark(const rbg_parser::parsed_game& pg, size_t iterations)
+{
+    game_description gd = create_description(pg);
 
     std::unordered_map<token_id_t, player_results> player_scores_sum;
 
-    for(const auto& token : pg->get_declarations().get_legal_players())
+    for(const auto& token : pg.get_declarations().get_legal_players())
     {
         std::string name = token.to_string();
         token_id_t player_id = gd.get_resolver().id(name);
@@ -105,9 +55,6 @@ int main(int argc,const char *argv[]) {
     }
 
     size_t turns = 0;
-    size_t iterations = 1;
-    if(vm.count("number"))
-        iterations = vm["number"].as<uint>();
     size_t avgmoves = 0;
     size_t all_moves_count = 0;
     auto begin = std::chrono::system_clock::now();
@@ -122,10 +69,8 @@ int main(int argc,const char *argv[]) {
         while (!moves.empty()) {
             if(iterations == 1)
                 std::cout << "\n";
-            volatile int unused = perft(&context, &state, search_depth);
             state.make_move(moves[rand() % moves.size()]);
             if(iterations == 1) {
-                std::cout << "Perft: " << unused << " (Depth: " << search_depth << ")" << std::endl;
                 std::cout << state << std::endl;
             }
             if(state.player() == state.get_description().get_keeper_player_id())
@@ -166,5 +111,78 @@ int main(int argc,const char *argv[]) {
         std::cout << "\t" << gd.get_resolver().name(player_score.first) << " : "
                   << (double) player_score.second.sum / iterations
                   << " (Min: " << player_score.second.min << ", Max: " << player_score.second.max << ")\n";
+    }
+}
+
+void perft_benchmark(const rbg_parser::parsed_game& pg, size_t depth)
+{
+    game_description gd = create_description(pg);
+    auto begin = std::chrono::system_clock::now();
+    search_context context;
+    game_state state(gd);
+    auto result = perft(&context, &state, depth);
+    auto end = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration<double>(end - begin).count();
+    std::cout << "Calculated perft for depth " << depth << " in " <<  duration << "s" << std::endl;
+    std::cout << "The result was: " << result << std::endl;
+}
+
+int main(int argc,const char *argv[]) {
+    po::options_description description("Allowed options");
+    description.add_options()
+            ("number,n",po::value<uint>(),"number of games to be played")
+            ("help,h","produce help message")
+            ("input-file,i",po::value<std::string>(),"input file")
+            ("randomseed,s",po::value<uint>(),"random seed for random player")
+            ("depth,d",po::value<uint>(),"depth of perft calculation")
+            ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc,argv,description), vm);
+    po::notify(vm);
+
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
+    po::store(po::command_line_parser(argc, argv).
+            options(description).positional(p).run(), vm);
+    po::notify(vm);
+
+    if(vm.count("randomseed")) {
+        srand(vm["randomseed"].as<uint>());
+    }
+    if(vm.count("help")) {
+        std::cout << description << std::endl;
+        return 1;
+    }
+
+    rbg_parser::messages_container msg;
+    std::ifstream t(vm["input-file"].as< std::string>());
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+
+    std::unique_ptr<rbg_parser::parsed_game> pg;
+
+    try {
+        std::vector<rbg_parser::token> result = tokenize(buffer.str(), msg);
+        rbg_parser::game_items g = input_tokens(result, msg);
+        pg = std::unique_ptr<rbg_parser::parsed_game>(new rbg_parser::parsed_game(g.parse_game(msg)));
+    }catch(rbg_parser::message& m){
+        std::cout<<"Game description is bad. Here is the error: " << "\n";
+        std::cout<<m.as_error()<<std::endl;
+        return 1;
+    }
+
+    if(vm.count("depth"))
+    {
+        size_t depth = vm["depth"].as<uint>();
+        perft_benchmark(*pg, depth);
+    }
+    else
+    {
+        size_t iterations = 1;
+        if(vm.count("number"))
+            iterations = vm["number"].as <uint>();
+        random_play_benchmark(*pg, iterations);
     }
 }
