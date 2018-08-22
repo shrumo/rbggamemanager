e5431b43a6897e6a7a45c8a41843e269238d1967
Marek Szykuła <marek.esz@gmail.com>
2018-08-22 18:19:28 +0200

    Updated benchmark script.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,5 +3,5 @@
 cd "${0%/*}"
 ./make.sh
 echo -e "\n\tChess"
-../build/benchmark ../rbgParser/examples/chess-opt.rbg -n 1000 -s 0
-../build/benchmark ../rbgParser/examples/chess-opt.rbg -s 0 -d 4
+../build/benchmark ../rbgParser/examples/chess.rbg -n 1000 -s 0
+../build/benchmark ../rbgParser/examples/chess.rbg -s 0 -d 4
