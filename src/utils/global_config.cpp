c13a6b9dc01884ac1bc78a5b711c7758e4cf2bfa
MSZ <marek.esz@gmail.com>
2018-08-27 20:36:24 +0200

    Shift table threshold set to 1.

--- src/utils/global_config.cpp
+++ src/utils/global_config.cpp
@@ -3,4 +3,4 @@
 //
 
 unsigned int kTextWidth = 2; // How much of the piece name is shown when printing the game board
-unsigned int kShiftTableClearLength = 2; // What is the length to use the ShiftTable optimization
+unsigned int kShiftTableClearLength = 1; // What is the length to use the ShiftTable optimization
