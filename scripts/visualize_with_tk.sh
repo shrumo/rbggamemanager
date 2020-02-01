130c293e8ba0413b0320a022efba878c3dd27764
Maksymilian Mika <shrum@oldie>
2020-02-01 17:09:03 +0000

    Update python bindings requirements

--- scripts/visualize_with_tk.sh
+++ scripts/visualize_with_tk.sh
@@ -2,5 +2,7 @@
 
 # This script guides user through visualizaing process on Linux systems.
 
+echo "This requires tkinter, make sure to install it. (sudo apt install python3-tk)" 
+
 ${0%/*}/./make.sh
 python3 ${0%/*}/../build/visualize_graph.py $1
