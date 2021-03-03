fcd0129085c5918178e474c849469db2a7511638
MSZ <marek.esz@gmail.com>
2021-03-03 20:11:05 +0100

    Benchmark with random simulations

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -5,6 +5,7 @@
 #include <game_state/construction/game_state_creator.h>
 #include <stl_extension/argparse.h>
 #include <utility/calculate_perft.h>
+#include <utility/calculate_sim.h>
 
 #include <cassert>
 #include <chrono>
@@ -17,16 +18,28 @@ int main(int argc, const char *argv[]) {
   auto args = std_ext::parse_args(argc, argv);
 
   if (args.positional_args.size() != 1) {
-    std::cout << "Usage: " << argv[0] << " <game_file> [--depth <search_depth>]"
+    std::cout << "Usage: " << argv[0] << " <game_file> [--depth <search_depth>] [--sim <sim_count>] [--seed <seed_uint>]"
               << std::endl;
     return 0;
   }
 
-  uint depth = 2;
+  uint depth = 0;
   if (args.flags.find("depth") != args.flags.end()) {
     depth = std::stoi(args.flags["depth"]);
   }
-
+  uint sim_count = 0;
+  if (args.flags.find("sim") != args.flags.end()) {
+    sim_count = std::stoi(args.flags["sim"]);
+  }
+  if (depth == 0 && sim_count == 0) {
+    std::cout << "Either depth or sim must be used" << std::endl;
+    return 1;
+  } else
+  if (depth > 0 && sim_count > 0) {
+    std::cout << "Depth and sim cannot be used together" << std::endl;
+    return 1;
+  }
+  
   std::ifstream filestream(args.positional_args[0]);
   std::stringstream buffer;
   buffer << filestream.rdbuf();
@@ -35,16 +48,41 @@ int main(int argc, const char *argv[]) {
   auto game = CreateGameState(buffer.str());
   auto end = std::chrono::system_clock::now();
   auto duration = std::chrono::duration<double>(end - begin).count();
-  std::cout << "Created the game state in in " << std::fixed << std::showpoint
-            << duration << "s" << std::endl;
-  begin = std::chrono::system_clock::now();
-  auto result = Perft(game, depth);
-  end = std::chrono::system_clock::now();
-  duration = std::chrono::duration<double>(end - begin).count();
-  std::cout << "Calculated perft for depth " << depth << " in " << std::fixed
-            << std::showpoint << duration << "s" << std::endl;
-  std::cout << "There are " << result.leaves_count << " leaves" << std::endl;
-  std::cout << "Number of traveled states: " << result.nodes_count << " ("
-            << std::fixed << std::showpoint << result.nodes_count / duration
-            << " states/sec)" << std::endl;
+  std::cout << "Created the game state in " << std::fixed << std::showpoint << duration << "s" << std::endl;
+
+  if (depth > 0) {
+    begin = std::chrono::system_clock::now();
+    auto result = Perft(game, depth);
+    end = std::chrono::system_clock::now();
+    duration = std::chrono::duration<double>(end - begin).count();
+    std::cout << "Calculated perft for depth " << depth << " in " << std::fixed
+              << std::showpoint << duration << "s" << std::endl;
+    std::cout << "There are " << result.leaves_count << " leaves" << std::endl;
+    std::cout << "Number of traveled states: " << result.nodes_count << " ("
+              << std::fixed << std::showpoint << result.nodes_count / duration
+              << " states/sec)" << std::endl;
+  } else {
+    uint seed;
+    if (args.flags.find("seed") != args.flags.end()) {
+      seed = std::stoi(args.flags["seed"]);
+    } else {
+      seed = std::random_device()();
+    }
+
+    begin = std::chrono::system_clock::now();
+    SimResult result = RandomSimulations(game, sim_count, seed);
+    end = std::chrono::system_clock::now();
+    duration = std::chrono::duration<double>(end - begin).count();
+    std::cout << "Calculated " << sim_count << " random simulations in " << std::fixed
+              << std::showpoint << duration << "s" << std::endl;
+    std::cout << "Number of traveled states: " << result.nodes_count << " ("
+              << std::fixed << std::showpoint << result.nodes_count / duration
+              << " states/sec)" << std::endl;
+    std::cout << "Average depth: " << static_cast<double>(result.nodes_count) / sim_count << std::endl;
+    std::cout << "Average goals:";
+    for (uint r = 0; r < result.sum_goals.size(); r++) {
+      std::cout << " " << static_cast<double>(result.sum_goals[r]) / sim_count;
+    }
+    std::cout << std::endl;
+  }
 }
---
+++ src/utility/calculate_sim.h
@@ -0,0 +1,49 @@
+#ifndef RBGGAMEMANAGER_CALCULATE_SIM_H
+#define RBGGAMEMANAGER_CALCULATE_SIM_H
+
+#include "game_state/game_state.h"
+#include "utility/printer.h"
+#define RBG_RANDOM_GENERATOR 0
+#include "rbg_random_generator.hpp"
+
+namespace rbg {
+
+  struct SimResult {
+    std::vector<unsigned long> sum_goals;
+    unsigned long nodes_count, sim_count;
+    
+    SimResult(uint players_count): nodes_count(0), sim_count(0) {
+        sum_goals.resize(players_count, 0);
+    }
+  };
+
+  SimResult RandomSimulations(GameState &state, uint sim_count, unsigned long seed) {
+    RBGRandomGenerator rng(seed);
+    std::vector<variable_id_t> player_variable_ids;
+    uint players_count = state.declarations().players_resolver().size()-1;
+    for (uint r = 0; r < players_count; r++) {
+      auto player_name = state.declarations().players_resolver().Name(r+1);
+      player_variable_ids.push_back(state.declarations().variables_resolver().Id(player_name));
+    }
+    SimResult result(players_count);
+    for (uint i = 0; i < sim_count; i++) {
+      uint depth = 0;
+      while (true) {
+        auto moves = state.Moves();
+        if (moves.size() == 0) break;
+        uint chosen_move = rng.uniform_choice(moves.size());
+        state.Apply(moves[chosen_move]);
+        depth++;
+      }
+      result.nodes_count += depth;
+      result.sim_count++;
+      for (uint rvi = 0; rvi < player_variable_ids.size(); rvi++)
+        result.sum_goals[rvi] += state.variables_values()[player_variable_ids[rvi]];
+      state.Reset();
+    }
+    return result;
+  }
+
+}
+
+#endif //RBGGAMEMANAGER_CALCULATE_SIM_H
