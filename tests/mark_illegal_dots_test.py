84af9045529174c3340a8947fac1c90e8f3d9cd6
Maksymilian Mika <mika.maksymilian@gmail.com>
2021-05-04 10:50:50 +0100

    Adding some more tests for the basic test for mark_illegal_dots.cc
    This still doesn't take into account modifiers correctly.

--- tests/mark_illegal_dots_test.py
+++ tests/mark_illegal_dots_test.py
@@ -18,8 +18,57 @@ with open(tmp_game_filename, 'w') as f:
 
 out = subprocess.getoutput(
     './mark_illegal_dots {}'.format(tmp_game_filename))
-# assert('#rules' in out)
-print('With illegal dots marked:')
+assert('#rules' in out)
+print('Simple case with illegal dots marked:')
 print(out)
 rules = out[out.find('#rules'):]
 rules = rules.replace(' ', '').replace('\n', '')
+assert(rules == "#rules=(->red(right+(/*illegal*/.right))->blue)")
+
+tmp_game_filename = 'tmp_game.rbg'
+tmp_game_text = '''
+#players = red(100), blue(50)
+#pieces = e, r, b
+#variables = turn(10)
+#board = rectangle(up, down, left, right,
+    [b,r]
+    [e,e])
+
+#rules = ->red (.left)* ->blue
+'''
+
+with open(tmp_game_filename, 'w') as f:
+    print(tmp_game_text, file=f)
+
+out = subprocess.getoutput(
+    './mark_illegal_dots {}'.format(tmp_game_filename))
+assert('#rules' in out)
+print('No illegal dots here:')
+print(out)
+rules = out[out.find('#rules'):]
+rules = rules.replace(' ', '').replace('\n', '')
+assert(rules == "#rules=(->red(.left)*->blue)")
+
+tmp_game_filename = 'tmp_game.rbg'
+tmp_game_text = '''
+#players = red(100), blue(50)
+#pieces = e, r, b
+#variables = turn(10)
+#board = rectangle(up, down, left, right,
+    [b,r]
+    [e,e])
+
+#rules = ->red (.left* + .right*) (.up* + .down*) ->blue
+'''
+
+with open(tmp_game_filename, 'w') as f:
+    print(tmp_game_text, file=f)
+
+out = subprocess.getoutput(
+    './mark_illegal_dots {}'.format(tmp_game_filename))
+assert('#rules' in out)
+print('No illegal dots here:')
+print(out)
+rules = out[out.find('#rules'):]
+rules = rules.replace(' ', '').replace('\n', '')
+assert(rules == "#rules=(->red((/*illegal*/.left*)+(/*illegal*/.right*))((/*illegal*/.up*)+(/*illegal*/.down*))->blue)")
