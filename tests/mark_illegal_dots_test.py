661ad12b65bd4620829c38743da945b97d896b90
Maksymilian Mika <mika.maksymilian@gmail.com>
2021-05-22 13:48:15 +0100

    Adding a more complicated test to mark_illegal_dots

--- tests/mark_illegal_dots_test.py
+++ tests/mark_illegal_dots_test.py
@@ -267,3 +267,33 @@ rules = out[out.find('#rules'):]
 rules = rules.replace(' ', '').replace('\n', '')
 assert(rules=="#rules=(->red(.down)*(left*+(/*illegal*/.right*))->blue)")
 
+tmp_game_filename = 'tmp_game.rbg'
+tmp_game_text = '''
+#players = red(100), blue(50)
+#pieces = e, r, b
+#variables = turn(10)
+#board = rectangle(up, down, left, right,
+    [e,e,e,e,e,e]
+    [e,e,e,e,e,e]
+    [e,e,e,e,e,e]
+    [e,e,e,e,e,e]
+    [e,e,e,e,e,e]
+    [e,e,e,e,e,e])
+
+#rules = ->red (. right + . right down + . right down^2 + . right down^2 left + . left
++ . left down + . left down^2 + . left right) ->blue
+'''
+
+with open(tmp_game_filename, 'w') as f:
+    print(tmp_game_text, file=f)
+
+out = subprocess.getoutput(
+    './mark_illegal_dots {}'.format(tmp_game_filename))
+# assert('#rules' in out)
+print('Those dots are legal:')
+print(out)
+rules = out[out.find('#rules'):]
+rules = rules.replace(' ', '').replace('\n', '')
+print(rules)
+assert(rules=="#rules=(->red((.right)+(.rightdown)+(.rightdowndown)+(.rightdowndownleft)+(.left)+(.leftdown)+(.leftdowndown)+(.leftright))->blue)")
+
